d0ed6acd229b2387f986a18e9c9db71db7fc2689
tphoney@gmail.com <tphoney@gmail.com@52cd0040-25ad-6b28-bf31-d0af7ba9c629>
2012-01-11 00:29:32 +0000

    Added in results screen, only displays text so far :)

--- trunk/src/com/android/MusicShareSyncActivity.java
+++ trunk/src/com/android/MusicShareSyncActivity.java
@@ -5,82 +5,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 
 public class MusicShareSyncActivity extends Activity {
 	/** Called when the activity is first created. */
 	// private Collection musicCollection = new Collection();
-	private static final int SETTINGS_SCREEN = Menu.FIRST;
-
-
+	private static final int SETTINGS_SCREEN = Menu.FIRST + 1;
+	private static final int MUSIC_SCREEN = Menu.FIRST;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.main);
-//		ImageView image = (ImageView) findViewById(R.drawable.background);
-//		setContentView(image);
-		
-		
-		// TextView tv = new TextView(this);
-		// tv.setText("Please set up the defaults.");
-		// setContentView(tv);
+		// ImageView image = (ImageView) findViewById(R.drawable.background);
+		// setContentView(image);
 
-		// CifsInteraction bla = new CifsInteraction();
-		// try {
-		// bla.createConnection(targetDomain, remoteUsername, remotePassword,
-		// remoteHostname);
-		// } catch (SmbException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// } catch (UnknownHostException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		// List<String> temp = new ArrayList<String>();
-		// try {
-		// temp = bla.getListOfDirs(remoteHostname, remoteBaseDirectory);
-		// } catch (MalformedURLException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// } catch (SmbException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		//
-		// tv.setText(temp.toString());
-		// boolean successful = false;
-		// try {
-		// successful = bla.copyFileTo(remoteHostname,
-		// "/Music/Adele/19/folder.jpg", localBaseDirectory);
-		// } catch (IOException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		// if (successful) {
-		// tv.setText("we just copied a fucking file");
-		// }
-		// setContentView(tv);
+		
 
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
+
+		menu.add(0, MUSIC_SCREEN, 0, "Start to Sync");
 		menu.add(0, SETTINGS_SCREEN, 0, "Settings");
 		return true;
 	}
 
 	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		Intent i;
 		switch (item.getItemId()) {
+		case MUSIC_SCREEN:
+			i = new Intent(MusicShareSyncActivity.this,
+					MusicScreenActivity.class);
+			startActivity(i);
+			return true;
 		case SETTINGS_SCREEN:
-			Intent i = new Intent(MusicShareSyncActivity.this,
+			i = new Intent(MusicShareSyncActivity.this,
 					SettingsScreenActivity.class);
 			startActivity(i);
 			return true;
-
 		}
 		return super.onMenuItemSelected(featureId, item);
 	}
