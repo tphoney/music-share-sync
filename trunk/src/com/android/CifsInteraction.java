81477f8828903e698e785e32e4624a9219a58c8d
tphoney@gmail.com <tphoney@gmail.com@52cd0040-25ad-6b28-bf31-d0af7ba9c629>
2012-01-27 20:22:52 +0000

    removing un-needed classes added in a progress spinner. it will have to do.

--- trunk/src/com/android/CifsInteraction.java
+++ trunk/src/com/android/CifsInteraction.java
@@ -3,7 +3,6 @@ package com.android;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -17,12 +16,9 @@ import jcifs.smb.SmbException;
 import jcifs.smb.SmbFile;
 import jcifs.smb.SmbFileInputStream;
 import jcifs.smb.SmbSession;
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Environment;
 
-class CifsInteraction extends AsyncTask<String, String, String> {
+class CifsInteraction  {
 	private NtlmPasswordAuthentication authentication;
 
 	public void createConnection(String domain, String username,
@@ -84,14 +80,18 @@ class CifsInteraction extends AsyncTask<String, String, String> {
 			SmbFile smbFile = new SmbFile("smb://" + srcHost
 					+ smbifyPath(remoteFilePath) + "/"
 					+ smbifyPath(remoteFileName), authentication);
+			long fileSize = smbFile.length(); 
 			SmbFileInputStream in = new SmbFileInputStream(smbFile);
 			BufferedInputStream bis = new BufferedInputStream(in);
 
 			// the actual copy
 			int byte_;
-
+			long copied = 0;
 			while ((byte_ = bis.read()) != -1) {
 				bos.write(byte_);
+				copied+= byte_;
+				int percentageComplete = (int)((copied*100)/fileSize);
+				//do something update progress bar
 			}
 
 			bos.close();
@@ -152,107 +152,6 @@ class CifsInteraction extends AsyncTask<String, String, String> {
 		returnVal = returnVal.replaceAll(host, "");
 		returnVal = returnVal + '/';
 		return returnVal;
-	}
-
-	private ProgressDialog dialog;
-	private Context context;
-
-	public void setContext(Context bla) {
-		context = bla;
-	}
-
-	// can use UI thread here
-	@Override
-	protected void onPreExecute() {
-		dialog = new ProgressDialog(context);
-		dialog.setMessage("Downloading file..");
-		dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-		dialog.setIndeterminate(false);
-
-		dialog.show();
-		super.onPreExecute();
-	}
-
-	@Override
-	protected String doInBackground(String... params) {
-		String srcHost = params[0];
-		String remoteFilePath = params[1];
-		String remoteFileName = params[2];
-		String localDir = params[3];
-
-		File root = Environment.getExternalStorageDirectory();
-		File localFilePath = new File(root.getPath() + "/" + localDir + "/"
-				+ remoteFilePath);
-		if (!localFilePath.exists()) {
-			// create folder structure if necessary
-			localFilePath.mkdirs();
-		}
-
-		if (localFilePath.canWrite()) {
-			// setup where we write too.
-			File outputFile = new File(localFilePath, remoteFileName);
-			try {
-				FileOutputStream fos = new FileOutputStream(outputFile);
-
-				BufferedOutputStream bos = new BufferedOutputStream(fos);
-
-				// set up where we read from
-				SmbFile smbFile = new SmbFile("smb://" + srcHost
-						+ smbifyPath(remoteFilePath) + "/"
-						+ smbifyPath(remoteFileName), authentication);
-
-				long fileSize = smbFile.length();
-
-				SmbFileInputStream in = new SmbFileInputStream(smbFile);
-				BufferedInputStream bis = new BufferedInputStream(in);
-
-				// the actual copy
-				int byte_;
-				long copied = 0;
-
-				while ((byte_ = bis.read()) != -1) {
-					bos.write(byte_);
-					copied += byte_;
-					int percent = (int) ((copied * 100) / fileSize);
-
-					publishProgress("" + percent);
-				}
-
-				bos.close();
-				in.close();
-
-			} catch (FileNotFoundException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SmbException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-		return null;
-	}
-
-	@Override
-	protected void onProgressUpdate(String... progress) {
-		dialog.setProgress(Integer.parseInt(progress[0]));
-	}
-
-	@Override
-	protected void onPostExecute(String unused) {
-
-		// super.onPostExecute(unused);
-		dialog.dismiss();
-
 	}
 
 }
