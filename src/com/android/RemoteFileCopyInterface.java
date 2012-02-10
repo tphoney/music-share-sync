2c3303a65de4acdc51bd72ee1e62c7fc2c9000b7
tphoney@gmail.com <tphoney@gmail.com>
2012-02-10 21:34:22 +0000

    Added in Interface to make copys more generic

---
+++ src/com/android/RemoteFileCopyInterface.java
@@ -0,0 +1,26 @@
+package com.android;
+
+import java.util.List;
+
+import android.os.Handler;
+
+public interface RemoteFileCopyInterface {
+	void createConnection(final String domain, final String username,
+			final String password, final String host) throws Exception;
+
+	List<String> getDirectoryContents(final String remoteFilePath)
+			throws Exception;
+
+	boolean isLeaf(final String remoteFilePath, final String itemClicked)
+			throws Exception;
+
+	void copyFileTo(final String remoteFilePath, final String fileToCopy,
+			final String destinationFolder, final Handler progresHandler)
+			throws Exception;
+
+	void copyFolder(final String remoteFilePath, final String folderToCopy,
+			final String destinationFolder, final Handler progressHandler)
+			throws Exception;
+
+	String getParent(final String folderName) throws Exception;
+}
